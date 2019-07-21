96487ea0e3a4658128e0e5a7a515dcfe8ce680b0
unknown <913088741@qq.com>
2019-07-21 22:14:02 +0800

    format code

--- SocketChat/index.js
+++ SocketChat/index.js
@@ -2,8 +2,10 @@ const events=require('events')
 const net=require('net')
 //node事件触发器
 const channel=new events.EventEmitter();
+
 channel.clients={}
 channel.subscriptions={};
+
 channel.on('join',function(id,client){
     channel.clients[id]=client
     channel.subscriptions[id]=(senderId,message)=>{
@@ -13,14 +15,15 @@ channel.on('join',function(id,client){
             channel.clients[id].write(message)
         }
     }
-    channel.on('broadcast',channel.subscriptions[id]);
+    channel.on('broadcast',channel.subscriptions[id]);//监听数据
 })
+//telnet ip port
 const server=net.createServer(client=>{
     const id=`${client.remoteAddress}:${client.remotePort}`;
     channel.emit('join',id,client);
     client.on('data',data=>{
         data=data.toString();
-        channel.emit('broadcast',id,data);
+        channel.emit('broadcast',id,data);//广播数据
     })
 })
 server.listen(8888)
